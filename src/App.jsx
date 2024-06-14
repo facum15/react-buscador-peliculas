726bee0ee9793ac7740f92cd2f90384bbb4248fc
Facundo Martinez <fmartinez@buk.cl>
2024-06-13 22:38:13 -0400

    mejorar legibilidad de codigo

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,28 @@ function App () {
   const movies = responseMovies.Search
   const hasMovies = movies?.length > 0
 
+  const renderMovies = () => {
+    return (
+      <ul>
+        {
+          movies.map(movie => (
+            <li key={movie.imbdID}>
+              <h3>{movie.Title}</h3>
+              <p>{movie.Year}</p>
+              <img src={movie.Poster} alt={movie.Title} />
+            </li>
+          ))
+        }
+      </ul>
+    )
+  }
+
+  const renderNoResults = () => {
+    return (
+      <p>No se encontraron resultados</p>
+    )
+  }
+
   return (
     <div className='page'>
 
@@ -20,22 +42,8 @@ function App () {
       <main>
         {
           hasMovies
-            ? (
-              <ul>
-                {
-                  movies.map(movie => (
-                    <li key={movie.imbdID}>
-                      <h3>{movie.Title}</h3>
-                      <p>{movie.Year}</p>
-                      <img src={movie.Poster} alt={movie.Title} />
-                    </li>
-                  ))
-                }
-              </ul>
-              )
-            : (
-              <p>No se encontraron resultados</p>
-              )
+            ? renderMovies()
+            : renderNoResults()
         }
       </main>
     </div>
