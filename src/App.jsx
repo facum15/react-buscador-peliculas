3cc3dab0f81782e4cdcd080927d2f8ad1c2f9100
Facundo Martinez <fmartinez@buk.cl>
2024-06-15 05:36:11 -0400

    cambio

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.css'
 import { Movies } from './components/Movies'
 import responseMovies from './mocks/with-results.json'
 // import withoutResults from './mocks/no-results.json'
-
 function App () {
   const movies = responseMovies.Search
 
