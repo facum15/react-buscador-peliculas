f9840acdc60ee72b4e53e26dbefe7c37cc04f6b0
Facundo Martinez <fmartinez@buk.cl>
2024-06-14 23:56:47 -0400

    Extraer componente Movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,10 @@
 import './App.css'
+import { Movies } from './components/Movies'
 import responseMovies from './mocks/with-results.json'
 // import withoutResults from './mocks/no-results.json'
 
 function App () {
   const movies = responseMovies.Search
-  const hasMovies = movies?.length > 0
-
-  const renderMovies = () => {
-    return (
-      <ul>
-        {
-          movies.map(movie => (
-            <li key={movie.imbdID}>
-              <h3>{movie.Title}</h3>
-              <p>{movie.Year}</p>
-              <img src={movie.Poster} alt={movie.Title} />
-            </li>
-          ))
-        }
-      </ul>
-    )
-  }
-
-  const renderNoResults = () => {
-    return (
-      <p>No se encontraron resultados</p>
-    )
-  }
 
   return (
     <div className='page'>
@@ -40,11 +18,7 @@ function App () {
       </header>
 
       <main>
-        {
-          hasMovies
-            ? renderMovies()
-            : renderNoResults()
-        }
+        <Movies movies={movies} />
       </main>
     </div>
   )
