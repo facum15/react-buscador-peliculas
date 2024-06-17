1a87d992275d993b7ce1a33c194b518dd2873e71
Facundo Martinez <fmartinez@buk.cl>
2024-06-17 18:32:55 -0400

    Usar useMemo para mejorar rendimiento y evitar ordenamiento por cada caracter ingresado. Evitar regerar la funcion getMovies en cada render con useMemo y pasando search como parámetro

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ function App () {
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    getMovies()
+    getMovies({ search })
   }
 
   const handleSort = () => {
--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react'
+import { useMemo, useRef, useState } from 'react'
 import { searchMovies } from '../services/movies'
 
 export function useMovies ({ search, sort }) {
@@ -7,25 +7,29 @@ export function useMovies ({ search, sort }) {
   const [error, setError] = useState(null)
   const previusSearch = useRef(search)
 
-  const getMovies = async () => {
-    if (search === previusSearch.current) return
+  const getMovies = useMemo(() => {
+    return async ({ search }) => {
+      if (search === previusSearch.current) return
 
-    try {
-      setLoading(true)
-      setError(null)
-      previusSearch.current = search
-      const newMovies = await searchMovies({ search })
-      setMovies(newMovies)
-    } catch (error) {
-      setError(error.message)
-    } finally {
-      setLoading(false)
+      try {
+        setLoading(true)
+        setError(null)
+        previusSearch.current = search
+        const newMovies = await searchMovies({ search })
+        setMovies(newMovies)
+      } catch (error) {
+        setError(error.message)
+      } finally {
+        setLoading(false)
+      }
     }
-  }
+  }, [])
 
-  const sortedMovies = sort
-    ? [...movies].sort((a, b) => a.title.localeCompare(b.title))
-    : movies
+  const sortedMovies = useMemo(() => {
+    return sort
+      ? [...movies].sort((a, b) => a.title.localeCompare(b.title))
+      : movies
+  }, [sort, movies])
 
   return { movies: sortedMovies, getMovies, loading }
 }
